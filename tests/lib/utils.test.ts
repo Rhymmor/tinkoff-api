0ab846d7d9ad334004d8b1cc731fa4e78cf27b6d
Rhymmor <anatolybelonog@gmail.com>
2019-02-11 10:58:45 +0700

    add tests for safeGet util function

--- tests/lib/utils.test.ts
+++ tests/lib/utils.test.ts
@@ -1,4 +1,4 @@
-import {isOk} from '../../src/lib/utils';
+import {isOk, safeGet} from '../../src/lib/utils';
 
 describe('Utils function tests', () => {
     test('isOk with positive check', () => {
@@ -12,5 +12,19 @@ describe('Utils function tests', () => {
     test('isOk with negative check', () => {
         expect(isOk(undefined)).toBe(false);
         expect(isOk(null)).toBe(false);
+    });
+
+    test("safeGet function doesn't fail", () => {
+        expect(safeGet({a: 1}, x => x.a)).toBe(1);
+        expect(safeGet(null, (x: any) => x.a.b.c)).toBe(undefined);
+        expect(safeGet(undefined, (x: any) => x.a.b.c)).toBe(undefined);
+    });
+
+    test("safeGet function gets default value", () => {
+        expect(safeGet(null, (x: any) => x.a.b.c, 1)).toBe(1);
+        expect(safeGet(undefined, (x: any) => x.a.b.c, 2)).toBe(2);
+
+        const obj: {a?: number} = {a: undefined};
+        expect(safeGet(obj, x => x.a, 2)).toBe(2);
     });
 });
